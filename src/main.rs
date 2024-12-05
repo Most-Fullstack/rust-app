1bea4ddf0b801d4a76e7af656a45fa1a9483b59c
Most Fullstack <most@hpktechnology.com>
2024-12-05 16:56:25 +0700

    Add dotenv dependency and configure environment variables in main application

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{web, App, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
 
 async fn greet() -> impl Responder {
     "Hello, world 0.1.1!"
@@ -10,16 +12,16 @@ fn health_check() {
 
 fn logging(port: &str, endpoint: &str, mode: &str) {
     println!("Server started at http://{}:{} in {} mode", endpoint, port, mode);
-    
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     health_check();
     dotenv().ok();
-    let Ok(mode) = env::var("PRINT_ENV")
-    let Ok(endpoint) = env::var("ENDPOINT")
-    let Ok(port) = env::var("PORT")
+
+    let mode = env::var("PRINT_ENV").unwrap_or_else(|_| "dev".to_string());
+    let endpoint = env::var("ENDPOINT").unwrap_or_else(|_| "0.0.0.0".to_string());
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
 
     logging(&port, &endpoint, &mode);
 
@@ -27,7 +29,8 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .route("/", web::get().to(greet))
     })
-    .bind("0.0.0.0:8080")?
+
+    .bind(format!("0.0.0.0:{}", port))?
     .run()
     .await
 }
