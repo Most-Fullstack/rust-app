32206214347db676716571bc7f361571651a0805
Most Fullstack <most@hpktechnology.com>
2024-12-05 16:50:58 +0700

    Add .env configuration and enhance logging in main application

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,21 @@ fn health_check() {
     println!("rust-app Started");
 }
 
+fn logging(port: &str, endpoint: &str, mode: &str) {
+    println!("Server started at http://{}:{} in {} mode", endpoint, port, mode);
+    
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     health_check();
+    dotenv().ok();
+    let Ok(mode) = env::var("PRINT_ENV")
+    let Ok(endpoint) = env::var("ENDPOINT")
+    let Ok(port) = env::var("PORT")
+
+    logging(&port, &endpoint, &mode);
+
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
